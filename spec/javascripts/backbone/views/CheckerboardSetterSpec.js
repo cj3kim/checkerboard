2a6906bbbfdab79603cfbfd360f3f40ac8cbdcf5
Chris Kim <cj3kim@gmail.com>
2013-05-26 16:26:35 -0700

    Setting up #template function in CheckerboardSetterSpec

--- spec/javascripts/backbone/views/CheckerboardSetterSpec.js
+++ spec/javascripts/backbone/views/CheckerboardSetterSpec.js
@@ -5,13 +5,21 @@ describe("Backbone Checkerboard Setter", function () {
   });
 
   describe("#initialize", function () {
-    it("initializes the form template", function () {
+    it("renders the form into the document", function () {
+    });
+  });
+
+  describe("#template", function () {
+    it("returns a form template", function () {
       var template = _.template("<form id='chckerboard'> \
           Horizontal Tiles: <input type='text' name='horizontal-tiles'><br> \
           Vertical Tile: <input type='text' name='vertical-tiles'> <br> \
           <input type='submit' value='Submit'> \
-        </form>");
+        </form>"
+      );
+
       expect(setter.template).toEqual(template);
+
     });
   });
   
