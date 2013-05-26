ead87a5d358c25230c93a74728e70a922bd7fbef
Chris Kim <cj3kim@gmail.com>
2013-05-26 15:39:09 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkerboard::Application.config.secret_token = '8bed758ca4c7445913cbd8a745a50128777aae94bffef671b972aed705ad2b6ba022a106581f7cf752f97a67ece625ca4100feffdd64c19a28709f02c02a2ea0'
