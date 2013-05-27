6ab777782f816c65b7ae6f998d9a9cfd71557823
Chris Kim <cj3kim@gmail.com>
2013-05-27 14:49:01 -0700

    Completed testing #loadMatrix

--- app/assets/javascripts/backbone/views/Checkerboard.js
+++ app/assets/javascripts/backbone/views/Checkerboard.js
@@ -5,41 +5,14 @@ var Checkerboard = Backbone.View.extend({
     this.verticalTiles = options.verticalTiles
     this.tiles = [];
 
-    this.setBoardWidth()
-    this.clearAndRenderBoard()
+    this.setBoardWidth();
+    this.clearAndRenderBoard();
+    this.loadTileMatrix();
   },
 
   el: "#checkerboard", 
 
   render: function () {
-    var booleanColorOption = 0;
-    var tiles = this.tiles;
-    var marker = null;
-    var $markerTemplate = null;
-
-    for(var index in tiles) {
-      var i = parseInt(index) + 1; //0 and 1 will screw up the modulus calculation
-      var tile = tiles[index]
-
-      var $tileTemplate = $(this.template());
-      $tileTemplate = this.colorElement(booleanColorOption,$tileTemplate);
-
-      if (tile.subject instanceof Marker) {
-        $tileTemplate.addClass("x-mark");
-        marker = tile.subject;
-        marker.coordinate = parseInt(index);
-      }
-
-      this.$el.append($tileTemplate);
-
-      booleanColorOption++;
-
-      if ( i % this.horizontalTiles === 0 && this.horizontalTiles % 2 === 0 ) {
-        booleanColorOption--;
-      }
-    }
-
-
   },
 
   setBoardWidth: function() {
@@ -52,6 +25,14 @@ var Checkerboard = Backbone.View.extend({
   clearAndRenderBoard: function () {
     this.$el.empty()
     this.render();
+  },
+
+  loadTileMatrix: function () {
+    var totalTiles = this.horizontalTiles * this.verticalTiles;
+
+    for(var i = 1; i <= totalTiles; i++) {
+      this.tiles.push(new Tile());
+    }
   }
 
 });
--- spec/javascripts/backbone/views/CheckerboardSpec.js
+++ spec/javascripts/backbone/views/CheckerboardSpec.js
@@ -1,21 +1,18 @@
 describe("Checkerboard", function () {
   beforeEach(function () {
     loadFixtures("home.html");
-
-    board = new Checkerboard({
-      horizontalTiles: 4, 
-      verticalTiles: 5
-    });
   });
 
   describe("#initialize", function() {
     it("should have the appropriate properties", function () {
-      expect(board.horizontalTiles).toEqual(4);
-      expect(board.verticalTiles).toEqual(5);
-      expect(board.tiles).toEqual(new Array());
+      var newCheckerboard = new Checkerboard({ horizontalTiles: 4, verticalTiles: 5 });
+
+      expect(newCheckerboard.horizontalTiles).toEqual(4);
+      expect(newCheckerboard.verticalTiles).toEqual(5);
+      expect(newCheckerboard.tiles.length).toEqual(20);
     });
 
-    it("should set the board width and render it", function () {
+    it("should set the width of board and render it", function () {
       spyOn(Checkerboard.prototype, "setBoardWidth");
       spyOn(Checkerboard.prototype, "clearAndRenderBoard");
 
@@ -25,7 +22,12 @@ describe("Checkerboard", function () {
       expect(newCheckerboard.clearAndRenderBoard).toHaveBeenCalled();
     });
 
-    it("should load the tiles array with Tile objects", function () {
+    it("should load tiles into the tiles property", function () {
+      spyOn(Checkerboard.prototype, "loadTileMatrix");
+
+      var newCheckerboard = new Checkerboard({horizontalTiles: 8, verticalTiles: 3});
+
+      expect(newCheckerboard.loadTileMatrix).toHaveBeenCalled();
 
     });
   });
@@ -33,21 +35,23 @@ describe("Checkerboard", function () {
   describe("#setBoardWidth", function () {
 
     it("sets the board to the appropriate width in pixels", function () {
+      var newCheckerboard = new Checkerboard({ horizontalTiles: 4, verticalTiles: 5 });
       var $checkerboard = $("#checkerboard");
       var expectedWidth = 200
-      board.setBoardWidth();
+
+      newCheckerboard.setBoardWidth();
 
       expect($checkerboard.width()).toBe(expectedWidth);
 
-      board.horizontalTiles = 5;
+      newCheckerboard.horizontalTiles = 5;
       expectedWidth = 250;
-      board.setBoardWidth();
+      newCheckerboard.setBoardWidth();
 
       expect($checkerboard.width()).toBe(expectedWidth);
 
-      board.horizontalTiles = 7;
+      newCheckerboard.horizontalTiles = 7;
       expectedWidth = 350;
-      board.setBoardWidth();
+      newCheckerboard.setBoardWidth();
 
       expect($checkerboard.width()).toBe(expectedWidth);
     });
@@ -55,12 +59,41 @@ describe("Checkerboard", function () {
 
   describe("#loadTileMatrix", function () {
     it("should load the tiles property with tile objects", function () {
-      var totalTiles;
-      totalTiles = board.horizontalTiles * board.verticalTiles
+      var totalTiles, firstTile, lastTile, 
+          lastTileIndex, arrayLength,newCheckerboard;
 
-      board.loadTileMatrix();
+      newCheckerboard = new Checkerboard({horizontalTiles: 3, verticalTiles: 2});
 
-      expect(board.tiles.length).toEqual(totalTiles);
+      totalTiles = newCheckerboard.horizontalTiles * newCheckerboard.verticalTiles
+      arrayLength = newCheckerboard.tiles.length;
+
+      firstTile = newCheckerboard.tiles[0];
+      lastTileIndex = arrayLength - 1;
+      lastTile = newCheckerboard.tiles[lastTileIndex];
+
+
+      expect(arrayLength).toEqual(totalTiles);
+      expect(firstTile instanceof Tile).toBe(true);
+      expect(lastTile instanceof Tile).toBe(true);
+      expect(lastTileIndex).toBe(5);
+      expect(arrayLength).toBe(6);
+
+      newCheckerboard = new Checkerboard({horizontalTiles: 5, verticalTiles: 5});
+
+      totalTiles = newCheckerboard.horizontalTiles * newCheckerboard.verticalTiles
+      arrayLength = newCheckerboard.tiles.length;
+
+      firstTile = newCheckerboard.tiles[0];
+      lastTileIndex = arrayLength - 1;
+      lastTile = newCheckerboard.tiles[lastTileIndex];
+
+
+      expect(arrayLength).toEqual(totalTiles);
+      expect(firstTile instanceof Tile).toBe(true);
+      expect(lastTile instanceof Tile).toBe(true);
+      expect(lastTileIndex).toBe(24);
+      expect(arrayLength).toBe(25);
+
     });
   });
 
