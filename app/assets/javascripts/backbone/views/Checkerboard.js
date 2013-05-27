04cc73f7bee01435ca982ae871c092b8596748c4
Chris Kim <cj3kim@gmail.com>
2013-05-26 17:57:57 -0700

    started coding the backbone view Checkerboard

---
+++ app/assets/javascripts/backbone/views/Checkerboard.js
@@ -0,0 +1,4 @@
+var Checkerboard = Backbone.View.extend({
+  initialize: function () {
+  }
+});
---
+++ spec/javascripts/backbone/views/CheckerboardSpec.js
@@ -0,0 +1,24 @@
+describe("Checkerboard", function () {
+  beforeEach(function () {
+    board = new Checkerboard();
+  });
+
+  describe("#initialize", function(){
+    it("should have horizonal and vertical tile counts", function () {
+      expect(board.horizontalTiles).toEqual(3)
+      expect(board.verticalTiles).toEqual(4)
+    });
+  });
+
+  describe("#",function(){
+  });
+
+  describe("#",function(){
+  });
+
+  describe("#",function(){
+  });
+
+
+
+});
